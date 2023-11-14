fe4e5106b195c3e8aaef8a415e096d3225c899c1
Junhong Chen <chenj297@mcmaster.ca>
2023-11-14 17:57:51 -0500

    import functions to the framework

--- GenPerfectTrip/chatroom/views.py
+++ GenPerfectTrip/chatroom/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .functions.send import send_prompt
 # Create your views here.
+
+
+
+
+def index(request):
+    hotels = send_prompt()
+    return render(request, template_name="index.html", context={"hotels": hotels})
+
