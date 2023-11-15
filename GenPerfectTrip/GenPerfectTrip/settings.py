dd7f417363c35dd68097ec4e299e6a0b24640f3d
Junhong Chen <chenj297@mcmaster.ca>
2023-11-14 20:17:17 -0500

    build rough interface

--- GenPerfectTrip/GenPerfectTrip/settings.py
+++ GenPerfectTrip/GenPerfectTrip/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "chatroom"
+    "chatroom",
+    "bootstrap5"
 ]
 
 MIDDLEWARE = [
--- GenPerfectTrip/chatroom/views.py
+++ GenPerfectTrip/chatroom/views.py
@@ -7,5 +7,6 @@ from .functions.send import send_prompt
 
 def index(request):
     hotels = send_prompt()
-    return render(request, template_name="index.html", context={"hotels": hotels})
+    # return render(request, template_name="index.html", context={"hotels": hotels})
+    return render(request, template_name="home.html", context={"hotels": hotels})
 
