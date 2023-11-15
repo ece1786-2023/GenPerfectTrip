95c5ca263f4c7a6180307688b439caded1a82a09
Junhong Chen <chenj297@mcmaster.ca>
2023-11-15 11:51:56 -0500

    improve uesr interface

--- GenPerfectTrip/chatroom/views.py
+++ GenPerfectTrip/chatroom/views.py
@@ -9,6 +9,7 @@ def hotels(request):
         user_input = request.POST.get('user_input')
         print(user_input)
         hotels = send_prompt_for_hotels(user_input)
+        hotels = "adfasdfadsfqaweof;ihajrAKLEWIGHAJDTGadfasdfadsfqaweof;ihajrAKLEWIGHAJDTGadfasdfadsfqaweof;ihajrAKLEWIGHAJDTGadfasdfadsfqaweof;ihajrAKLEWIGHAJDTGadfasdfadsfqaweof;ihajrAKLEWIGHAJDTG"
         return render(request, template_name="home.html", context={"outputs": hotels, "raw_user_input": user_input})
     return render(request, template_name="home.html")
 
